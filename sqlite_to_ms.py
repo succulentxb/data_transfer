46d0cc0efc7b02fbe484daacd739c88d6d4668dc
刘欣彪 <liuxinbiao@liuxinbiaodeMacBook-Pro.local>
2018-10-24 20:27:35 +0800

    complete sqlite_to_ms

--- sqlite_to_ms.py
+++ sqlite_to_ms.py
@@ -9,9 +9,40 @@ ms_cursor = ms_db.cursor()
 sqli_db = sqli.connect('database.db')
 sqli_cursor = sqli_db.cursor()
 
-# copy data in table room
-sqli_cursor.execute('select * from room;')
+# copy data in room table
+print('start copy data in room.')
+sqli_cursor.execute('SELECT * FROM room;')
 data_list = sqli_cursor.fetchall()
 # print(data)
 for data in data_list:
-    query = 'INSERT INTO room ('
+    query = 'INSERT INTO room (kdno, kcno, ccno, kdname, exptime, papername) VALUES ('
+    for content in data:
+        query += '"' + content + '", '
+    query = query.strip(' ')
+    query = query.strip(',')
+    query += ');'
+    # print(query)
+    try:
+        ms_cursor.execute(query)
+        ms_db.commit()
+    except:
+        ms_db.rollback()
+print('copy done.')
+
+# copy data in student table
+print('start copy data in student.')
+sqli_cursor.execute('SELECT * FROM student;')
+data_list = sqli_cursor.fetchall()
+for data in data_list:
+    query = 'INSERT INTO student (registno, name, kdno, kcno, ccno, seat) VALUES ('
+    for content in data:
+        query += '"' + content + '", '
+    query = query.strip(' ')
+    query = query.strip(',')
+    query += ');'
+    try:
+        ms_cursor.execute(query)
+        ms_db.commit()
+    except:
+        ms_db.rollback()
+print('copy done.')
