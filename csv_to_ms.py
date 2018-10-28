19a2e511344d6ab7290cf026d511242578806f86
刘欣彪 <liuxinbiao@liuxinbiaodeMacBook-Pro.local>
2018-10-28 12:26:57 +0800

    complete the document

--- csv_to_ms.py
+++ csv_to_ms.py
@@ -2,8 +2,8 @@ import csv
 import pymysql as pm 
 import db_tools as dt 
 
-#dt.clear_db()
-#dt.create_table()
+# dt.clear_db()
+# dt.create_table()
 
 db = dt.connection()
 cursor = db.cursor()
@@ -15,11 +15,22 @@ csvreader = csv.reader(csvfile, delimiter=',')
 data_list = []
 for row in csvreader:
     data_list.append(row)
-# delete the first header row
+
+# store the header of table, to construct sql dynamically
+headers = data_list[0]
 data_list = data_list[1:]
-# print(data_list)
+
+# construct the predix of sql command
+query_predix = 'INSERT INTO room ('
+for header in headers:
+    query_predix += (header + ', ')
+# rid the ', ' at the end of query_predix
+query_predix = query_predix.strip(' ')
+query_predix = query_predix.strip(',')
+query_predix += ') VALUES ('
+
 for data in data_list:
-    query = 'INSERT INTO room (kdno, kcno, ccno, kdname, exptime, papername) VALUES ('
+    query = query_predix
     for content in data:
         query += '"' + content + '", '
     query = query.strip(' ')
@@ -39,14 +50,24 @@ csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 data_list = []
 for row in csvreader:
     data_list.append(row)
+
+headers = data_list[0]
 data_list = data_list[1:]
+
+query_predix = 'INSERT INTO student ('
+for header in headers:
+    query_predix += (header + ', ')
+query_predix = query_predix.strip(' ')
+query_predix = query_predix.strip(',')
+query_predix += ') VALUES ('
 for data in data_list:
-    query = 'INSERT INTO student (registno, name, kdno, kcno, ccno, seat) VALUES ('
+    query = query_predix
     for content in data:
         query += '"' + content + '", '
     query = query.strip(' ')
     query = query.strip(',')
     query += ');'
+    # print(query)
     try:
         cursor.execute(query)
         db.commit()
--- sqlite_to_ms.py
+++ sqlite_to_ms.py
@@ -1,23 +1,34 @@
 import pymysql as pm 
 import sqlite3 as sqli 
 import db_tools as dt 
+import configparser
 
-dt.clear_db()
-dt.create_table()
+# dt.clear_db()
+# dt.create_table()
 
 ms_db = dt.connection()
 ms_cursor = ms_db.cursor()
 
-sqli_db = sqli.connect('database.db')
+conf = configparser.ConfigParser()
+conf.read('db.conf')
+sqli_db = sqli.connect(conf['sqlite_db']['db'])
 sqli_cursor = sqli_db.cursor()
 
-# copy data in room table
-print('start copy data in room.')
-sqli_cursor.execute('SELECT * FROM room;')
+# copy data in student table
+print('start copy data in student.')
+sqli_cursor.execute('PRAGMA table_info ("student");')
+header_raw = sqli_cursor.fetchall()
+headers = [row[1] for row in header_raw]
+query_predix = 'INSERT INTO student ('
+for header in headers:
+    query_predix += (header + ', ')
+query_predix = query_predix.strip(' ')
+query_predix = query_predix.strip(',')
+query_predix += ') VALUES ('
+sqli_cursor.execute('SELECT * FROM student;')
 data_list = sqli_cursor.fetchall()
-# print(data)
 for data in data_list:
-    query = 'INSERT INTO room (kdno, kcno, ccno, kdname, exptime, papername) VALUES ('
+    query = query_predix
     for content in data:
         query += '"' + content + '", '
     query = query.strip(' ')
@@ -31,17 +42,31 @@ for data in data_list:
         ms_db.rollback()
 print('copy done.')
 
-# copy data in student table
-print('start copy data in student.')
-sqli_cursor.execute('SELECT * FROM student;')
+# copy data in room table
+print('start copy data in room.')
+# read the header of table room and construct a predix of query
+sqli_cursor.execute('PRAGMA table_info ("room");')
+header_raw = sqli_cursor.fetchall()
+headers = [row[1] for row in header_raw]
+headers[4] = 'exptime'
+query_predix = 'INSERT INTO room ('
+for header in headers:
+    query_predix += (header + ', ')
+query_predix = query_predix.strip(' ')
+query_predix = query_predix.strip(',')
+query_predix += ') VALUES ('
+# read the content in table
+sqli_cursor.execute('SELECT * FROM room;')
 data_list = sqli_cursor.fetchall()
+# print(data)
 for data in data_list:
-    query = 'INSERT INTO student (registno, name, kdno, kcno, ccno, seat) VALUES ('
+    query = query_predix
     for content in data:
         query += '"' + content + '", '
     query = query.strip(' ')
     query = query.strip(',')
     query += ');'
+    # print(query)
     try:
         ms_cursor.execute(query)
         ms_db.commit()
